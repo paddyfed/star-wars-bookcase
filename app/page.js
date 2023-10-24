bf737f23378c40a3bd107b579a579c31c2bf6c93
Paddy <paddyfedoo@hotmail.com>
2023-10-24 21:46:54 +0100

    Api on Index page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getData } from "./helpers/apiHelpers";
 
-export default function Home() {
-  return "Hello";
+export default async function Home() {
+  const api = `${process.env.API_URL}api`;
+
+  const data = await getData(api);
+
+  return (
+    <>
+      {data.bookcase.map((book) => {
+        return <div key={book.id}>{book.id}</div>;
+      })}
+    </>
+  );
 }
