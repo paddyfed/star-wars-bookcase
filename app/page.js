c68d599173a0e910a38de9c63244d60579939e94
Paddy <paddyfedoo@hotmail.com>
2023-10-25 21:56:43 +0100

    Ordered by Release Date

--- app/page.js
+++ app/page.js
@@ -3,14 +3,18 @@ import styles from "./page.module.css";
 import { getData } from "./helpers/apiHelpers";
 
 export default async function Home() {
-  const api = `${process.env.API_URL}api`;
+  const api = `${process.env.API_URL}/books/release-date`;
 
   const data = await getData(api);
 
   return (
     <>
       {data.bookcase.map((book) => {
-        return <div key={book.id}>{book.id}</div>;
+        return (
+          <div key={book.id}>
+            {book.id} {book.ReleaseDate}
+          </div>
+        );
       })}
     </>
   );
