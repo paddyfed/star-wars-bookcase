835c1e2b81697fe722e3ed1299870bab448ec9be
Paddy <paddyfedoo@hotmail.com>
2023-10-26 20:42:59 +0100

    Start of timeline order - not implemented yet

---
+++ app/api/books/timeline-order/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import bookcase from "@/json/bookcase.json";
+
+export async function GET() {
+  const ordered = bookcase.bookcase;
+  ordered.sort((a, b) => (a.releaseDate > b.releaseDate ? 1 : -1));
+  return NextResponse.json({ bookcase: ordered });
+}
