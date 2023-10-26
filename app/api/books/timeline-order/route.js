33768c7754455a6453d6826bea5b2c6681fe908b
Paddy <paddyfedoo@hotmail.com>
2023-10-26 22:24:09 +0100

    Timeline Order

--- app/api/books/timeline-order/route.js
+++ app/api/books/timeline-order/route.js
@@ -1,8 +1,24 @@
 import { NextResponse } from "next/server";
 import bookcase from "@/json/bookcase.json";
 
+// Create an empty array to hold the ordered books
+const orderedBooks = [];
+
+// Find the book with no "prev" value (start book)
+const startBook = bookcase.bookcase.find((book) => book.prev === null);
+
+// Start with the "start book" and follow the "next" values to order the books
+let currentBook = startBook;
+while (currentBook) {
+  orderedBooks.push(currentBook);
+  currentBook = bookcase.bookcase.find((book) => book.id === currentBook.next);
+}
+
+// console.log("bookList", orderedBooks);
+// console.log("bookMap", bookMap);
+
 export async function GET() {
-  const ordered = bookcase.bookcase;
-  ordered.sort((a, b) => (a.releaseDate > b.releaseDate ? 1 : -1));
-  return NextResponse.json({ bookcase: ordered });
+  //const ordered = bookcase.bookcase;
+  // ordered.sort((a, b) => (a.releaseDate > b.releaseDate ? 1 : -1));
+  return NextResponse.json({ bookcase: orderedBooks });
 }
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const styleClassses = {
 };
 
 export default async function Home() {
-  const api = `${process.env.API_URL}/books/release-date`;
+  const api = `${process.env.API_URL}/books/timeline-order`;
 
   const data = await getData(api);
 
