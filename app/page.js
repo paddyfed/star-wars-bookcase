69363d6e0b97b542644ec4021888d3fa8974a064
Paddy <paddyfedoo@hotmail.com>
2023-10-31 23:07:18 +0000

    SVG Image

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getData } from "./helpers/apiHelpers";
+import StarWarsIcon from "../public/images/Star_Wars_Logo.svg";
 
 const styleClassses = {
   novel: styles.novel,
@@ -12,19 +13,32 @@ export default async function Home() {
   const data = await getData(api);
 
   return (
-    <div className={styles.bookcase}>
-      {data.bookcase.map((book) => {
-        return (
-          <div
-            key={book.id}
-            className={`${styles.book} ${styleClassses[book.type]}`}
-          >
-            <div className={styles.series}>{book.series}</div>
-            <div>{book.title}</div>
-            <div className={styles.author}>{book.author}</div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.bookcase}>
+        {data.bookcase.map((book) => {
+          return (
+            <div
+              key={book.id}
+              className={`${styles.book} ${styleClassses[book.type]}`}
+            >
+              <div>
+                <Image
+                  src={StarWarsIcon}
+                  width={50}
+                  height={100}
+                  alt="Star Wars"
+                  className={styles.image}
+                />
+              </div>
+              <div className={styles.bookText}>
+                <div className={styles.series}>{book.series}</div>
+                <div className={styles.title}>{book.title}</div>
+                <div className={styles.author}>{book.author}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
