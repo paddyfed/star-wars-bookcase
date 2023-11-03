d4f7d606fcbfcb9e365e412f3567968823086c65
Paddy <paddyfedoo@hotmail.com>
2023-11-03 23:02:52 +0000

    rename JSON

--- app/api/books/canon/release-date/route.js
+++ app/api/books/canon/release-date/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bookcase from "@/json/canon-bookcase.json";
+import bookcase from "@/json/canonbookcase.json";
 
 export const dynamic = "force-dynamic";
 
