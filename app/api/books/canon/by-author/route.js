af9fe81172a3e6ada606da751ef14cdedd3d7ae1
Paddy <paddyfedoo@hotmail.com>
2023-11-09 21:19:24 +0000

    Deploy By Author API endpoints

---
+++ app/api/books/canon/by-author/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import bookcase from "@/json/canonbookcase.json";
+import { bookCaseByAuthor } from "@/app/helpers/apiHelpers";
+
+const ordered = bookCaseByAuthor(bookcase);
+
+export async function GET() {
+  return NextResponse.json({ bookcase: ordered });
+}
--- app/helpers/apiHelpers.js
+++ app/helpers/apiHelpers.js
@@ -30,3 +30,20 @@ export function bookCaseReleaseOrder(bookcase) {
   const ordered = bookcase.bookcase;
   return ordered.sort((a, b) => (a.releaseDate > b.releaseDate ? 1 : -1));
 }
+
+export function bookCaseByAuthor(bookcase) {
+  const ordered = bookcase.bookcase;
+  const returnArr = ordered
+    .sort((a, b) => (a.author > b.author ? 1 : -1)) // sort by author name
+    .reduce((acc, book) => {
+      // group by author name also - returns an array of objects using the author name
+      const author = book.author;
+      if (!acc[author]) {
+        acc[author] = [];
+      }
+      acc[author].push(book);
+      return acc;
+    }, {});
+
+  return returnArr;
+}
