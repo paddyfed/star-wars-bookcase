b59168f27e2aaa78d4ebc11f61031878f87800b1
Paddy <paddyfedoo@hotmail.com>
2023-11-28 21:57:36 +0000

    Added bought toggle to release date

--- app/components/timelineOrderBookCase.js
+++ app/components/timelineOrderBookCase.js
@@ -7,16 +7,37 @@ import CombinedBookCase from "./combinedBookCase";
 
 export default function TimeLineOrderBookCase({ data }) {
   const [show, setShow] = useState(false);
+  const [showNotBought, setShowNotBought] = useState(true);
 
   function toggle() {
     setShow(!show);
   }
+  function toggleNotBought() {
+    setShowNotBought(!showNotBought);
+    const hello = document.querySelectorAll(`.${styles.notbought}`);
+    console.log(hello);
+    hello.forEach((item) => {
+      item.classList.toggle(styles.hidden);
+    });
+  }
+
   return (
     <>
       <div className={styles.toggleSwitch}>
         <input type="checkbox" id="switch" onChange={toggle} checked={show} />
         <label htmlFor="switch" className={styles.toggleSwitchLabel}>
           Show Publishing Eras
+        </label>
+      </div>
+      <div className={styles.toggleSwitch}>
+        <input
+          type="checkbox"
+          id="switchNotBought"
+          onChange={toggleNotBought}
+          checked={showNotBought}
+        />
+        <label htmlFor="switchNotBought" className={styles.toggleSwitchLabel}>
+          Show Bought Books
         </label>
       </div>
       {show ? <SplitBookCase data={data} /> : <CombinedBookCase data={data} />}
