17b7cb1126169e2d7de28ea74579b849d841dc15
Paddy <paddyfedoo@hotmail.com>
2024-01-30 00:19:45 +0000

    Revalidate API 1 hour

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
+export const revalidate = 3600;
+
 export const metadata = {
   title: {
     template: "Star Wars Bookcase - %s",
