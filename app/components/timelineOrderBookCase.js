b81f8c8bc14528484def12dba5a34ff782662378
Paddy <paddyfedoo@hotmail.com>
2024-01-31 23:15:50 +0000

    Timeline Screen Toggle Changes

--- app/components/timelineOrderBookCase.js
+++ app/components/timelineOrderBookCase.js
@@ -4,49 +4,24 @@ import { useState } from "react";
 import styles from "@/app/page.module.css";
 import SplitBookCase from "./splitBookCase";
 import CombinedBookCase from "./combinedBookCase";
+import FilterToggle from "./filterToggle";
 
 export default function TimeLineOrderBookCase({ data, title }) {
   const [show, setShow] = useState(false);
   const [onlyShowBoughtBooks, setOnlyShowBoughtBooks] = useState(false);
 
-  function toggle() {
-    setShow(!show);
-  }
-  function toggleOnlyShowBoughtBooks() {
-    setOnlyShowBoughtBooks(!onlyShowBoughtBooks);
-  }
-
   return (
     <>
       <header className={styles.toggleSwitchHeader}>
         {title && <h2>{title}</h2>}
-        <div>
-          <span className={styles.toggleSwitch}>
-            <input
-              type="checkbox"
-              id="switch"
-              onChange={toggle}
-              checked={show}
-            />
-            <label htmlFor="switch" className={styles.toggleSwitchLabel}>
-              Show Publishing Eras
-            </label>
-          </span>
-          <span className={styles.toggleSwitch}>
-            <input
-              type="checkbox"
-              id="onlyShowBoughtBooksCheckBox"
-              onChange={toggleOnlyShowBoughtBooks}
-              checked={onlyShowBoughtBooks}
-            />
-            <label
-              htmlFor="onlyShowBoughtBooksCheckBox"
-              className={styles.toggleSwitchLabel}
-            >
-              Only show bought books
-            </label>
-          </span>
-        </div>
+        <FilterToggle
+          onlyShowBoughtBooks={onlyShowBoughtBooks}
+          show={show}
+          setShow={() => setShow(!show)}
+          setOnlyShowBoughtBooks={() =>
+            setOnlyShowBoughtBooks(!onlyShowBoughtBooks)
+          }
+        />
       </header>
       {show ? (
         <SplitBookCase data={data} onlyShowBoughtBooks={onlyShowBoughtBooks} />
